eab43aa928052069b771f863bf0c86992624a86f
NicolasNasoESIEE <nicolas.naso@edu.esiee.fr>
2024-09-18 10:30:13 +0200

    modif .py

--- GameEngine.py
+++ GameEngine.py
@@ -7,23 +7,69 @@ SCREEN_TITLE = "ChronoBlade"
 class GameEngine(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.ASH_GREY)
+        arcade.set_background_color(arcade.color.BLUE)
 
         self.player_x = 400
         self.player_y = 300
         self.player_speed = 5
+        self.gravity = -0.5
+        self.jump_strength = 10
+        self.player_velocity_y = 0
+        self.is_jumping = False
+        self.is_on_ground = False
 
         self.up_pressed = False
         self.down_pressed = False
         self.left_pressed = False
         self.right_pressed = False
 
+        self.platforms = [
+            arcade.SpriteSolidColor(200, 20, arcade.color.RED)
+            for _ in range(2)
+        ]
+        self.platforms[0].center_x = 400
+        self.platforms[0].center_y = 50
+        self.platforms[1].center_x = 300
+        self.platforms[1].center_y = 250
+
+        self.platform_direction = 1
+
+        self.ground = arcade.SpriteSolidColor(SCREEN_WIDTH, 20, arcade.color.BLACK)
+        self.ground.center_x = SCREEN_WIDTH // 2
+        self.ground.center_y = 30
+
+        self.camera = None
+
+    def setup(self):
+         self.camera = arcade.Camera(self.width, self.height)
+
+
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("test", 100, 500, arcade.color.BLACK, 20)
-        arcade.draw_circle_filled(self.player_x, self.player_y, 30, arcade.color.BLUE)
+        self.ground.draw()
+        for platform in self.platforms:
+            platform.draw()
+        arcade.draw_circle_filled(self.player_x, self.player_y, 30, arcade.color.GREEN)
+        self.camera.use()
 
     def on_update(self, delta_time):
+        if self.up_pressed and self.is_on_ground:
+            self.player_velocity_y = self.jump_strength
+            self.is_jumping = True
+            self.is_on_ground = False
+        
+        self.player_velocity_y += self.gravity
+        self.player_y += self.player_velocity_y
+
+        if self.player_y <= 30:
+            self.player_y = 30
+            self.is_on_ground = True
+            self.player_velocity_y = 0
+            self.is_jumping = False
+
+        self.check_platform_collisions()
+        self.move_platforms(delta_time)
+
         if self.up_pressed:
             self.player_y += self.player_speed
         if self.down_pressed:
@@ -32,6 +78,48 @@ class GameEngine(arcade.Window):
             self.player_x -= self.player_speed
         if self.right_pressed:
             self.player_x += self.player_speed
+
+        self.center_camera_to_player()
+
+    def center_camera_to_player(self):
+        screen_center_x = self.player_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_y - (
+            self.camera.viewport_height / 2
+        )
+
+        # Don't let camera travel past 0
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+
+    def check_platform_collisions(self):
+        self.is_on_ground = False
+        for platform in self.platforms:
+            if (self.player_y - 30 <= platform.center_y + 10 and
+                self.player_y - 30 >= platform.center_y - 10 and
+                self.player_x >= platform.center_x - 100 and
+                self.player_x <= platform.center_x + 100):
+                self.is_on_ground = True
+                self.player_y = platform.center_y + 30
+                self.player_velocity_y = 0
+                self.is_jumping = False
+
+        if self.player_y <= 30:
+            self.player_y = 30
+            self.is_on_ground = True
+            self.player_velocity_y = 0
+            self.is_jumping = False
+
+
+    def move_platforms(self, delta_time):
+        for platform in self.platforms:
+            platform.center_x += self.platform_direction * 100 * delta_time
+            if platform.center_x > SCREEN_WIDTH - platform.width // 2 or platform.center_x < platform.width // 2:
+                self.platform_direction *= -1
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.UP:
@@ -55,6 +143,7 @@ class GameEngine(arcade.Window):
 
 def main():
     window = GameEngine(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
     arcade.run()
 
 if __name__ == "__main__":
--- GameEngineArcade.py
+++ GameEngineArcade.py
@@ -13,35 +13,41 @@ TILE_SCALING = 0.5
 PLAYER_MOVEMENT_SPEED = 5
 GRAVITY = 1
 PLAYER_JUMP_SPEED = 20
+TIME_SLOW_FACTOR = 0.2
 
 class GameEngine(arcade.Window):
-    """
-    Main application class.
-    """
-
     def __init__(self):
-
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         self.scene = None
         self.player_sprite = None
+        self.sword_sprite = None
         self.physics_engine = None
         self.camera = None
-        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+        self.background = arcade.load_texture("Assets/main_background.png")
+        self.time_slowed = False
 
     def setup(self):
         self.camera = arcade.Camera(self.width, self.height)
         self.scene = arcade.Scene()
         self.scene.add_sprite_list("Player")
         self.scene.add_sprite_list("Walls", use_spatial_hash=True)
+        self.scene.add_sprite_list("Sword")
+
         player_sprite = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(player_sprite, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 96
         self.scene.add_sprite("Player", self.player_sprite)
 
+        sword_sprite = "Assets/sword.png"
+        self.sword_sprite = arcade.Sprite(sword_sprite, 0.5)
+        self.sword_sprite.center_x = self.player_sprite.center_x + 40
+        self.sword_sprite.center_y = self.player_sprite.center_y
+        self.scene.add_sprite("Sword", self.sword_sprite)
+
         for x in range(0, 1250, 64):
-            wall = arcade.Sprite("assets/Ground.png", TILE_SCALING)
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
             wall.center_x = x
             wall.center_y = 32
             self.scene.add_sprite("Walls", wall)
@@ -49,9 +55,7 @@ class GameEngine(arcade.Window):
         coordinate_list = [[512, 96], [256, 96], [768, 96]]
 
         for coordinate in coordinate_list:
-            wall = arcade.Sprite(
-                ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
-            )
+            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
             wall.position = coordinate
             self.scene.add_sprite("Walls", wall)
 
@@ -60,14 +64,12 @@ class GameEngine(arcade.Window):
         )
 
     def on_draw(self):
-        """Render the screen."""
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.clear()
         self.camera.use()
         self.scene.draw()
 
     def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed."""
-
         if key == arcade.key.UP or key == arcade.key.W:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = PLAYER_JUMP_SPEED
@@ -77,13 +79,25 @@ class GameEngine(arcade.Window):
             self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
-        """Called when the user releases a key."""
-
         if key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
 
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.time_slowed = True
+            arcade.schedule(self.restore_time, 1.5)
+            arcade.set_update_rate(TIME_SLOW_FACTOR)
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.time_slowed = False
+
+    def restore_time(self, delta_time):
+        if self.time_slowed:
+            arcade.set_update_rate(1/60)
+            self.time_slowed = False
 
     def center_camera_to_player(self):
         screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
@@ -96,19 +110,15 @@ class GameEngine(arcade.Window):
         self.camera.move_to(player_centered)
 
     def on_update(self, delta_time):
-        """Movement and game logic"""
-
         self.physics_engine.update()
         self.center_camera_to_player()
-
-
+        self.sword_sprite.center_x = self.player_sprite.center_x + 40
+        self.sword_sprite.center_y = self.player_sprite.center_y
 
 def main():
-    """Main function"""
     window = GameEngine()
     window.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
