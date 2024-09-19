e19efd72c9e027f6250052c8e5e47b959c79d5ae
KyroFrCode <adchayan937@live.fr>
2024-09-19 13:05:21 +0200

    add map edior and sprite

---
+++ Map_Editor/spritesheets/QR_spritesheet3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="QR_spritesheet3" tilewidth="32" tileheight="32" tilecount="1444" columns="38">
+ <image source="spritesheet_3.png" trans="000000" width="1225" height="1230"/>
+</tileset>
---
+++ Map_Editor/spritesheets/QR_spritesheet6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="QR_spritesheet6" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="spritesheet_6.png" trans="000000" width="1024" height="1024"/>
+</tileset>
